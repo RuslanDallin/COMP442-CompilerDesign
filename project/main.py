6ee75b139a5b85e1fec3bebdc312563f8c89b6a6
RuslanDallin <ruslan.dallin@outlook.com>
2022-01-26 01:02:45 -0500

    5

--- project/main.py
+++ project/main.py
@@ -1,10 +1,9 @@
-import math
-
 import pandas as pd
 
 table = pd.read_csv("Table.csv")
 table.set_index("State", inplace=True)
 file = open('lexnegativegrading.src', 'r')
+
 
 class Token:
     def __init__(self, type, lexeme, location):
@@ -12,7 +11,15 @@ class Token:
         self.lexeme = lexeme
         self.location = location
 
-# operators = ['!', '&', ',', '-', '.', '/', ':', ';', '<', '=', '>', '[', '|', ']', '_', '+', '.', '.', '.', '.', '.', '.',]
+
+LexemeDic = {"==": "eq", "+": "plus", "(": "openpar", ";": "semi", "<>": "noteq", "-": "minus",
+             "&": "and", ")": "closepar", ",": "comma", "<": "lt", "*": "multi", "!": "not", "{": "opencubr",
+             ".": "dot", ">": "gt", "/": "div", "}": "closecubr", ":": "colon", "<=": "leq", "=": "assign",
+             "[": "opensqbr", "::": "coloncplon", ">=": "geg", "]": "closesqbr", "->": "arrow", }
+
+reservedWords = ["if", "then", "else", "integer", "float", "void", "public", "private", "func", "var",
+                 "struct", "while", "func", "read", "write", "return", "self", "inherits", "let", "impl"]
+
 
 def type(c):
     if c.isalpha(): return 'L'
@@ -20,33 +27,19 @@ def type(c):
     if c.isdigit(): return 'D'
     return c
 
-def getState(state,c):
-    if c in table.columns:
-        temp = str(table[c][state])
-        if temp == "nan":
-            return 0 #no transitions
-        else:
-            return temp
-    return -1  #symbol not supported
 
-def getLabel(state):
-    temp = str(table["final"][state])
-    if temp == "nan":
-        return 0 #no transitions
-    else:
-        return temp
-
-def getInfo(state,c):
+def getInfo(state, c):
     tempState = -1  # char not supported
     tempLabel = -1
     if c in table.columns:
         tempState = str(table[c][state])
         if tempState == "nan":
-            tempState = 0   # no transations
+            tempState = 0  # no transations
         tempLabel = str(table["final"][state])
         if tempLabel == "nan":
-            tempLabel = 0   # not a final state
+            tempLabel = 0  # not a final state
     return str(tempState), str(tempLabel)
+
 
 def isComplete(state):
     tempLabel = str(table["final"][state])
@@ -54,8 +47,6 @@ def isComplete(state):
         tempLabel = 0  # not a final state
     return str(tempLabel)
 
-reservedWords = ["if", "then", "else", "integer", "float", "void", "public", "private", "func", "var",
-                 "struct", "while", "func", "read", "write", "return", "self", "inherits", "let", "impl"]
 
 def nextToken():
     while True:
@@ -79,11 +70,12 @@ def nextToken():
                 continue
 
             typeChar = type(char)
-            if ((state == 'A' or state == 'AR' or state == 'AT') and char == '0') or (state == 'AR' and char == 'e'): # if there is a 0 with nothing around, it should be considered 0 not digit
+            if ((state == 'A' or state == 'AR' or state == 'AT') and char == '0') or (
+                    state == 'AR' and char == 'e'):  # if there is a 0 with nothing around, it should be considered 0 not digit
                 typeChar = char
-            nextState, label = getInfo(state,typeChar)
+            nextState, label = getInfo(state, typeChar)
 
-            if state == 'Z': # // comments
+            if state == 'Z':  # // comments
                 while (char != '\n'):
                     token += char
                     char = file.read(1)
@@ -130,7 +122,6 @@ def nextToken():
                 tokenReady = False
                 continue
 
-
             if nextState == "-1" and label == "-1":
                 if char == '\n':
                     continue
@@ -145,7 +136,7 @@ def nextToken():
 
             if (nextState == "0" and label != "0") or (label == "id" and token in reservedWords):
                 print(char, typeChar, nextState, label)
-                print("Token",token)
+                print("Token", token)
                 state = 'A'
                 token = ""
                 file.seek(file.tell() - 1)  # go back
@@ -158,62 +149,6 @@ def nextToken():
             token = token + char
             state = nextState
             print(char, typeChar, nextState, label)
-
-
-
-
-
-
-
-
-# def nextToken():
-#     while True:
-#         state = 'A'
-#         token = ""
-#         label = None
-#         tokenReady = False
-#         while (True):
-#             char = file.read(1)
-#             if not char:
-#                 return None
-#             if char == '' or char == '\t':
-#                 continue
-#             typeChar = type(char)
-#             newState = str(getState(state,typeChar))
-#             if newState.isalpha():
-#                 state = newState
-#                 newLabel = str(getLabel(state))
-#                 if newLabel != 0:
-#                     label = newLabel
-#                     if tokenReady:
-#                         # print(char, end="")
-#                         # print(" ", state, label, tokenReady)
-#                         # print("Token Created")
-#                         # state = 'A'
-#                         # token = None
-#                         # label = None
-#                         # tokenReady = False
-#                         # continue
-#                          pass
-#                     tokenReady = True
-#                     token = token + char
-#
-#             elif newState == '0':
-#                 print(char, end="")
-#                 print(" ", state, label, tokenReady)
-#                 print("********Token Created",token)
-#                 state = 'A'
-#                 token = ""
-#                 label = None
-#                 tokenReady = False
-#                 file.seek(file.tell() -1) #go back
-#                 continue
-#
-#             print(char, end="")
-#             print(" ", state, label, tokenReady)
-
-
-
 
 
 print(nextToken())
