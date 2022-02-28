d53029ea2c8782d2369a204c792a7d51ad3fbcd2
RuslanDallin <ruslan.dallin@outlook.com>
2022-02-28 00:15:08 -0500

    ID replacement

--- project/Parser.py
+++ project/Parser.py
@@ -11,32 +11,36 @@ prodStack = []
 def isTerminal(s):
     return (s in LexemeDic.values()) or (s in reservedWords) or (s == "id")
 
-def getTableReversedRHS(row,col,PrevDeriv):
+def getTableReversedRHS(row,token,PrevDeriv):
     rhs= -1  # -1: no column match
     newDeriv = PrevDeriv
+    col = token.type
     if col in ParseTable.columns:
         rhs = str(ParseTable[col][row])
         if rhs == "nan":
             rhs = 0  # 0: cell is empty
         else:
-            newDeriv = derivationBuilder(clearDerivation(rhs),PrevDeriv)
+            newDeriv = derivationBuilder(derivationKeywordReplace(rhs),PrevDeriv) # updates derivation
             arrowIndex = rhs.index("→ ") + 2
             trucatedRhs = rhs[arrowIndex:]
             rhs = reverseSentence(trucatedRhs) + " "
     if rhs == -1:
-        cols = ParseTable.columns
-        for c in cols:
-            value = ParseTable[c][row]
-            if str(value) != "nan":
-                print(row,",", c, " => ", value)
+        printTransitions(row) # For debugging purposes
     return str(rhs), newDeriv
+
+def printTransitions(row):
+    cols = ParseTable.columns
+    for c in cols:
+        value = ParseTable[c][row]
+        if str(value) != "nan":
+            print(row, ",", c, " => ", value)
 
 def reverseSentence(s):
     tempList = s.split()
     reversedList = reversed(tempList)
     return " ".join(reversedList)
 
-def clearDerivation(unlearDerivation):
+def derivationKeywordReplace(unlearDerivation):
     clearDeri = ""
     list = unlearDerivation.split(" ")
     for e in list:
@@ -47,8 +51,8 @@ def clearDerivation(unlearDerivation):
         clearDeri += e + " "
     return clearDeri
 
-def derivationBuilder(s, PrevDeriv):
-    lhs, rhs = s.split("→")
+def derivationBuilder(newTransition, PrevDeriv):
+    lhs, rhs = newTransition.split("→")
     if rhs.strip() == "&epsilon":
         rhs = ''
     newStr = PrevDeriv.replace(lhs.strip(),rhs.strip(),1)
@@ -62,15 +66,17 @@ def parse(lexA):
         token = lexA.nextToken()
     error = False
     deriviation = "PROG"
-    deriviationList = []
+    progDerivation = []
     errorList = []
-    deriviationList.append(deriviation)
+    progDerivation.append(deriviation)
 
     while prodStack[-1] != "START":
         top = prodStack[-1]
         if isTerminal(top):
             if top == token.type:
                 deleted = prodStack.pop()
+                if deleted == "id": # replaced ID with the actual lexme in the derivation
+                    deriviation = deriviation.replace("id", token.lexeme, 1)
                 token = lexA.nextToken()
                 while token.type == "inlinecmt" or token.type == "blockcmt":
                     token = lexA.nextToken()
@@ -82,8 +88,8 @@ def parse(lexA):
                 error = True
                 break
         else:
-            tableEntry, deriviation = getTableReversedRHS(top,token.type,deriviation)
-            deriviationList.append(deriviation)
+            tableEntry, deriviation = getTableReversedRHS(top,token,deriviation)
+            progDerivation.append(deriviation)
             if (tableEntry == "0"):
                 print("\n--------------------------------")
                 print("Error encoundered in the grammar")
@@ -111,8 +117,8 @@ def parse(lexA):
         print("Col: ", top, "row: ", token.type, " tableEntry: ", tableEntry)
         errorList.append("syntax error at: " + str(token.location))
         print("--------------------------------")
-        return False, deriviationList, errorList
+        return False, progDerivation, errorList
     else:
-        return True, deriviationList, errorList
+        return True, progDerivation, errorList
 
 
