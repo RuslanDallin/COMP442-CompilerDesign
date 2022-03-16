c7c020ef9cf7eebd779bfca0f2b5d748823d70af
RuslanDallin <ruslan.dallin@outlook.com>
2022-03-16 18:30:55 -0400

    new branch

--- project/ASTdriver.py
+++ project/ASTdriver.py
@@ -9,7 +9,6 @@ directoryName = "Input" # set the source folder
 directory = os.listdir(directoryName)
 
 def astDriver():
-    # TO BE DELETED -----------------------------------------------------
     directoryName = "Input"  # set the source folder
     directory = os.listdir(directoryName)
     for file in directory:
@@ -28,6 +27,15 @@ def astDriver():
                     astOutput.write(str("%s%s: %s\n" % (pre, node.name, node.token.lexeme)))
                 else:
                     astOutput.write(str("%s%s\n" % (pre, node.name)))
+
+            for pre, fill, node in RenderTree(ast):
+                if node.name == "id" or node.name == "num" or node.name == "float" \
+                        or node.name == "sign" or node.name == "type" \
+                        or node.name == "visibility" or node.name.endswith("Op"):
+                    print("%s%s: %s" % (pre, node.name, node.token.lexeme))
+                else:
+                    print("%s%s" % (pre, node.name))
+
             print(parseCheck)
     # ---------------------------------------------------------------------
 
