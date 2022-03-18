fd7564b60d09b2e4aa7ed5652dfff7f6e490109e
RuslanDallin <ruslan.dallin@outlook.com>
2022-03-17 22:32:00 -0400

    small var working tree

--- project/testNew.py
+++ project/testNew.py
@@ -5,44 +5,85 @@ from Nodes import *
 class Visitor():
     def visit(self, node):
         if type(node) is varDeclSubtree:
-            print("visiting varDeclSubtree")
+            varName = node.children[0].data
+            varType = node.children[1].data
+            varDimlist = list()
+            for child in node.children[2].children:
+                varDimlist.append(child.data)
+            print("=>>",varName,varType,varDimlist)
+            entry = Entry("local", varName, varType, varDimlist)
+            print(entry)
+
+
         if type(node) is typeNode:
             print("visiting typeNode")
+
         if type(node) is IdNode:
             print("visiting IdNode")
 
-class symbolTable():
+        if type(node) is dimListSubtree:
+            print("visiting dimListSubtree")
 
-    # => varDeclSubtree
-    # => memberDeclListSubtree
-    # - local/param (param if present in memberDeclListSubtree)
-    # - name
-    # - type
-    # - dimList
-    # - private/public (only if memberDeclListSubtree)
-    def addEntry(self, category, id, type):
-        if category == None:
-            self.cate
+        if type(node) is numNode:
+            print("visiting numNode")
 
-    # => funcBodySubtree
-    # => memberDeclListSubtree
-    # - name
-    # - Entry (per variable)
-    def addTable(self): pass
+class symbolTable(): pass
 
-    # => funcDefSubtree
-    # - name
-    # - parms (or empty if main)
-    # - returnType (or empty if main)
-    # - private/public (only if Class)
-    # - Table
-    def addFunction(self): pass
+#TODO 1) inherList 2) functionList?
+
+# => varDeclSubtree
+# => memberDeclListSubtree
+# - category:
+#       - data if in memberDeclListSubtree
+#       - local
+#       - param if present in memberDeclListSubtree)
+# - name
+# - type
+# - dimList
+# - visibility: private/public (only if memberDeclListSubtree)
+class Entry():
+    def __init__(self, category, id, type, dimList=None, visibility=None):
+        self.category = category
+        self.id = id
+        self.type = type
+        self.dimList = dimList
+        if self.dimList:
+            self.dimList = ""
+            for dim in dimList:
+                self.dimList += "[" + str(dim) + "]"
+        self.visibility = visibility
+
+    def __str__(self):
+        if self.dimList and self.visibility:
+            return "%s\t | %s\t | %s%s\t | %s" % (self.category, self.id, self.type, self.dimList, self.visibility)
+        if self.dimList:
+            return "%s\t | %s\t | %s%s\t" % (self.category, self.id, self.type, self.dimList)
+        if self.visibility:
+            return "%s\t | %s\t | %s\t | %s\t" % (self.category, self.id, self.type, self.visibility)
+        else:
+            return "%s\t | %s\t | %s\t" % (self.category, self.id, self.type)
+
+# => funcBodySubtree
+# => memberDeclListSubtree
+# - name
+# - Entry (per variable)
+class Table():
+    def __init__(self): pass
+
+# => funcDefSubtree
+# - name
+# - parms (or empty if main)
+# - returnType (or empty if main)
+# - private/public (only if Class)
+# - Table
+class Function():
+    def __init__(self): pass
 
 
-    # - inheritance list
-    # - data (data +
-    # - function(s)
-    def addClass(self): pass
+# - inheritance list
+# - data (data +
+# - function(s)
+def Class(self): pass
 
 
 class BaseNode():
@@ -53,6 +94,9 @@ class BaseNode():
         self.symEntry = None
 
     def accept(self, visitor):
+        for child in self.children:
+            print("\tcalling ", child.name, "'s visit")
+            child.accept(visitor)
         visitor.visit(self)
 
 
@@ -63,26 +107,13 @@ class varDeclSubtree(BaseNode, Node):
         if children:
             self.children = children
 
-    def accept(self, visitor):
-        for child in self.children:
-            print("calling ", child.name, "'s visit")
-            child.accept(visitor)
-        print("called ", self.name, "'s visit")
-        visitor.visit(self)
-
-
 class typeNode(BaseNode, Node):
     def __init__(self, token=None, type=None):
         super(typeNode, self).__init__()
         self.name = "type"
         self.token = token
         if type:
-            self.type = type
-
-    def accept(self, visitor):
-        print("called ", self.name, "'s visit")
-        visitor.visit(self)
-
+            self.data = type
 
 class IdNode(BaseNode, Node):
     def __init__(self, token=None, id=None):
@@ -90,16 +121,31 @@ class IdNode(BaseNode, Node):
         self.name = "id"
         self.token = token
         if id:
-            self.type = id
+            self.data = id
 
-    def accept(self, visitor):
-        print("called ", self.name, "'s visit")
-        visitor.visit(self)
+class dimListSubtree(BaseNode, Node):
+    def __init__(self, children=None):
+        super(dimListSubtree, self).__init__()
+        self.name = "dimList"
+        if children:
+            self.children = children
+
+class numNode(BaseNode, Node):
+    def __init__(self, token=None, num=None):
+        super(numNode, self).__init__()
+        self.name = "num"
+        self.token = token
+        self.data = num
+        if num:
+            self.data = num
 
 
 n1 = IdNode(id="x")
 n2 = typeNode(type="float")
-n3 = varDeclSubtree((n1, n2))
+n31 = numNode(num=3)
+n3 = dimListSubtree((n31,))
+n4 = varDeclSubtree((n1, n2, n3))
+
 
 
 
@@ -109,7 +155,13 @@ print("PLAN FOR TODAY \n 1) try to make the tables symbTabes witout visitor \n \
 
 visitor = Visitor()
 
-print(n3.accept(visitor))
+n4.accept(visitor)
 
-for pre, fill, node in RenderTree(n3):
+for pre, fill, node in RenderTree(n4):
     print("%s%s" % (pre, node.name))
+
+# en = Entry("local", "n", "integer", visibility="private")
+# en = Entry("local", "n", "integer", (2,1))
+# en2 = Entry("local", "n", "integer", ())
+# print(en)
+# print(en2)
